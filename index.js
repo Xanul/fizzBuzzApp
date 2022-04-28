5e978474d4e1f925c9cf23035393684d86043daf
Xanul <rivas.060@gmail.com>
2022-04-27 22:08:52 -0500

    Fixed the ExplorerService.js class to return correct usernames format

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const ExplorerService = require("./lib/services/ExplorerService");
 const Reader = require("./lib/utils/Reader");
 
 const explorers = Reader.readJsonFile("./data/explorers.json")
-console.log(explorers)
+// console.log(explorers)
+
+const nodeExplorers = ExplorerService.filterByMission(explorers, "node")
+// console.log(nodeExplorers)
+
+const nodeGitHubUsernames = ExplorerService.getExplorersUsernamesByMission(explorers, "java")
+console.log(nodeGitHubUsernames)
