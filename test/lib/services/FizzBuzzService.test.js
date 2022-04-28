33a82acc5beceea3bf654387c3cc45e42e9887c8
Xanul <rivas.060@gmail.com>
2022-04-28 14:39:59 -0500

    Added tests for FizzBuzzService.js method

---
+++ test/lib/services/FizzBuzzService.test.js
@@ -0,0 +1,21 @@
+const FizzBuzzService = require("../../../lib/services/FizzBuzzService")
+const Reader = require("../../../lib/utils/Reader")
+
+describe('Testing the FizzBuzzService class', () => {
+
+    const explorers = Reader.readJsonFile('./data/explorers.json')
+    const explorersFizzBuzz = explorers.map((explorer)=> {
+        return FizzBuzzService.applyValidationInExplorer(explorer)
+    })
+
+    test('Testing applyValidationInExplorer method', () => {
+    
+      expect(explorersFizzBuzz.length).toBe(15)
+      expect(explorersFizzBuzz[0]).toMatchObject({trick: 1})
+      expect(explorersFizzBuzz[2]).toMatchObject({trick:"FIZZ"})
+      expect(explorersFizzBuzz[4]).toMatchObject({trick: 'BUZZ'})
+      expect(explorersFizzBuzz[14]).toMatchObject({trick: "FIZZBUZZ"})
+        
+    })
+
+})
