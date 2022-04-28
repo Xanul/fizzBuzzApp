d9911fe751fe8e804d7102cee1db9c8600fbad53
Xanul <rivas.060@gmail.com>
2022-04-27 21:57:22 -0500

    Added the test for Reader.js

---
+++ test/lib/utils/Reader.test.js
@@ -0,0 +1,8 @@
+const Reader = require("../../../lib/utils/Reader");
+
+describe('Testing the Reader.js file', () => {
+    test('Testing reading a file', () => {
+        const explorers = Reader.readJsonFile('./data/explorers.json')
+        expect(explorers).toBeDefined()
+    })
+})
