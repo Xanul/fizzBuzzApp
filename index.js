485fb1f331aedadb363cbdc1400033ba2df3dfa4
Xanul <rivas.060@gmail.com>
2022-04-29 15:17:38 -0500

    Added the ExplorerController.js class with static functions to interact with the Server

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+const ExplorerController = require("./lib/controllers/ExplorerController");
 const ExplorerService = require("./lib/services/ExplorerService");
 const Reader = require("./lib/utils/Reader");
 
-const explorers = Reader.readJsonFile("./data/explorers.json");
-// console.log(explorers)
+// const explorers = Reader.readJsonFile("./data/explorers.json");
+// // console.log(explorers)
 
-const nodeGitHubUsernames = ExplorerService.getExplorersUsernamesByMission(explorers, "java");
-console.log(nodeGitHubUsernames);
+// const nodeGitHubUsernames = ExplorerService.getExplorersUsernamesByMission(explorers, "java");
+// console.log(nodeGitHubUsernames);
+
+const test = ExplorerController.getExplorersByMission("node")
+// console.log(test)
+const test2 = ExplorerController.getExplorersUsernamesByMission("node")
+// console.log(test2)
+const test3 = ExplorerController.getExplorersAmountByMission("node")
+console.log(test3)
