cec9a9774c14b3173d8461a365d7288e4a5c983b
Xanul <rivas.060@gmail.com>
2022-04-29 20:12:34 -0500

    Added the applyValidationInNumber to FizzBuzzService

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const ExplorerController = require("./lib/controllers/ExplorerController");
 const ExplorerService = require("./lib/services/ExplorerService");
+const FizzBuzzService = require("./lib/services/FizzBuzzService");
 const Reader = require("./lib/utils/Reader");
 
 // const explorers = Reader.readJsonFile("./data/explorers.json");
@@ -13,4 +14,6 @@ const test = ExplorerController.getExplorersByMission("node")
 const test2 = ExplorerController.getExplorersUsernamesByMission("node")
 // console.log(test2)
 const test3 = ExplorerController.getExplorersAmountByMission("node")
-console.log(test3)
+// console.log(test3)
+const test4 = FizzBuzzService.applyValidationInNumber(15);
+console.log(test4)
